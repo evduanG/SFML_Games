9759445b8bde0e30b1fb9b97678722ecc3677126
evduanG <eviatar.duany@gmail.com>
2022-04-19 10:29:16 +0300

    uplodd TWL ,Timber

---
+++ TWL/Thomas.h
@@ -0,0 +1,10 @@
+#include "PlayableCharacter.h" 
+
+class	Thomas :	public PlayableCharacter 
+{ 
+public:  
+	// A constructor specific to Thomas   
+	Thomas();  
+	// The overridden input handler for Thomas  
+	bool virtual handleInput(); 
+};
